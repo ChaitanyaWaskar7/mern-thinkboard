041b95a24c17f8987c2f43b1c177e35b2be877fd
Chaitanya waskar <waskarchaitanya@gmail.com>
2025-06-07 18:27:49 +0530

    latest changes

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,36 @@
 import express from "express";
 import { connectDb } from "./src/config/db.js";
-import cors from "cors"
+import cors from "cors";
+
+import path from "path";
 
 import noteRoutes from "./src/routes/noteRoutes.js";
 import rateLimiter from "./src/middleware/rateLimiter.js";
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 // middleware for getting input in json format
-app.use(express.json())
-app.use(cors({
-  origin:"http://localhost:5173"
-}))
-app.use(rateLimiter)
+app.use(express.json());
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
+}
+app.use(rateLimiter);
 
 
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-// custom middleware 
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+
+
+// custom middleware
 // app.use((req,res,next) => {
 //   console.log("just got req");
 //   next();
@@ -25,10 +38,8 @@ app.use(rateLimiter)
 
 app.use("/api/notes", noteRoutes);
 
-
-
 connectDb().then(() => {
   app.listen(PORT, () => {
     console.log("Server is running on port :", PORT);
   });
-})
+});
